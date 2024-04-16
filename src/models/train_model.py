993e6a39d43e896e832fa2e5687adf6a8164ffc3
Theo Sourget <sourget.theo@live.fr>
2024-04-16 12:19:10 +0200

    add logging + evaluation to training process

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -12,8 +12,8 @@ from carbontracker.tracker import CarbonTracker
 
 import torch
 import torch.optim as optim
-from torchvision.models import resnet50
-from torch.nn.functional import softmax
+from torchvision.models import resnet50,densenet121
+from torch.nn.functional import sigmoid
 from sklearn.metrics import roc_auc_score,f1_score
 
 from torch.utils.tensorboard import SummaryWriter
@@ -22,39 +22,68 @@ from torch.utils.data import DataLoader
 from sklearn.model_selection import GroupShuffleSplit, GroupKFold
 from src.data.pytorch_dataset import MaskingDataset
 
-def training_epoch(model,criterion,optimizer,train_dataloader):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+def training_epoch(model,criterion,optimizer,train_dataloader):
+    model.to(DEVICE)
     model.train()
     train_loss = 0.0
+    lst_labels = []
+    lst_preds = []
+    
     for i, data in enumerate(train_dataloader, 0):
         inputs, labels = data
-        inputs,labels = inputs.float().to(device), torch.Tensor(np.array(labels).T).float().to(device)
+        inputs,labels = inputs.float().to(DEVICE), torch.Tensor(np.array(labels).T).float().to(DEVICE)
+
         # zero the parameter gradients
         optimizer.zero_grad()
         # forward + backward + optimize
         outputs = model(inputs)
+            
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
         train_loss += loss.item()
-        output_softmax = softmax(outputs)
-        if i == 0:
-            print(softmax(outputs)>0.5,labels)
-            print(f1_score(labels.cpu().detach().numpy(),(output_softmax.cpu().detach().numpy()>0.5),average=None))
-    return train_loss
+        output_sigmoid = sigmoid(outputs)
+        lst_labels.extend(labels.cpu().detach().numpy())
+        lst_preds.extend(output_sigmoid.cpu().detach().numpy()>0.5)
+    lst_labels = np.array(lst_labels)
+    lst_preds = np.array(lst_preds)
+    f1_scores=f1_score(lst_labels,lst_preds,average=None)
+    print("train",f1_scores,np.sum(lst_labels,axis=0),np.sum(lst_preds,axis=0),np.sum(lst_labels*lst_preds,axis=0))
 
-def valid_epoch(model,valid_data):
-    return 0,0
+    return train_loss/lst_labels.shape[0],f1_scores
+
+def valid_epoch(model,criterion,valid_dataloader):
+    model.to(DEVICE)
+    model.eval()
+    val_loss = 0.0
+    lst_labels = []
+    lst_preds = []
+    with torch.no_grad():
+        for i, data in enumerate(valid_dataloader, 0):
+            inputs, labels = data
+            inputs,labels = inputs.float().to(DEVICE), torch.Tensor(np.array(labels).T).float().to(DEVICE)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item()
+            output_sigmoid = sigmoid(outputs)
+            lst_labels.extend(labels.cpu().detach().numpy())
+            lst_preds.extend(output_sigmoid.cpu().detach().numpy()>0.5)
+        lst_labels = np.array(lst_labels)
+        lst_preds = np.array(lst_preds)
+        f1_scores=f1_score(lst_labels,lst_preds,average=None)
+        print("val",f1_scores,np.sum(lst_labels,axis=0),np.sum(lst_preds,axis=0),np.sum(lst_labels*lst_preds,axis=0))
+    return val_loss/lst_labels.shape[0],f1_scores
 
 def main():
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     #Get hyperparameters 
     NB_EPOCHS = int(os.environ.get("NB_EPOCHS"))
     NB_FOLDS = int(os.environ.get("NB_FOLDS"))
     BATCH_SIZE = int(os.environ.get("BATCH_SIZE"))
     LEARNING_RATE = float(os.environ.get("LEARNING_RATE"))
     CLASSES = os.environ.get("CLASSES").split(",")
+    base_run_name = f'runs/{datetime.now().strftime("%b_%d_%Y_%H%M%S")}'
 
     #Load the base dataset
     training_data = MaskingDataset(data_dir="./data/processed")
@@ -72,20 +101,11 @@ def main():
     testing_data.img_paths = np.array(testing_data.img_paths)[test_idx]
     testing_data.roi_paths = np.array(testing_data.roi_paths)[test_idx]
     
-
-    #Define model, loss and optimizer
-    model = resnet50(weights='DEFAULT') #Weights pretrained on imagenet_1k
-    kernel_count = model.fc.in_features
-    model.fc = torch.nn.Linear(kernel_count, len(CLASSES))
-    model.to(device)
-    criterion = torch.nn.CrossEntropyLoss()
-    criterion.requires_grad = True
-    
-    optimizer = optim.Adam(model.parameters(),lr=LEARNING_RATE)
-
     #Create k-fold for train/val
     group_kfold = GroupKFold(n_splits=NB_FOLDS)
     for i, (train_index,val_index) in enumerate(group_kfold.split(training_data.img_labels, groups= training_data.img_labels['PatientID'])):
+        
+        writer = SummaryWriter(f'{base_run_name}/Fold{i}')
         train_data = MaskingDataset(data_dir="./data/processed")
         train_data.img_labels = training_data.img_labels.iloc[train_index].reset_index(drop=True)
         train_data.img_paths = np.array(training_data.img_paths)[train_index]
@@ -98,18 +118,58 @@ def main():
 
         train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE)
         valid_dataloader = DataLoader(val_data, batch_size=BATCH_SIZE)
-
+        
+        
+        #Compute class weights to counter data imbalenced
+        y = np.array(train_data.img_labels["Onehot"].tolist())
+        #Define model, loss and optimizer
+        model = densenet121(weights='DEFAULT')#Weights pretrained on imagenet_1k
+        kernel_count = model.classifier.in_features
+        model.classifier = torch.nn.Linear(kernel_count, 2)
+        model.to(DEVICE)
+        
+        #weights = torch.as_tensor(np.sum(y==0,axis=0)/np.sum(y,axis=0),dtype=torch.float).to(DEVICE)
+        weights = torch.as_tensor([1,1],dtype=torch.float).to(DEVICE)
+        criterion = torch.nn.BCEWithLogitsLoss(pos_weight=weights)
+        criterion.requires_grad = True
+        
+        optimizer = optim.Adam(model.parameters(),lr=LEARNING_RATE)
         #Training epoch process
         for epoch in tqdm(range(NB_EPOCHS)):
+            # if i == 0 and epoch == 0:
+            #     tracker = CarbonTracker(epochs=NB_EPOCHS*NB_FOLDS,stop_and_confirm=False,components="gpu")
+            #     tracker.epoch_start()
             #Training
-            loss = training_epoch(model,criterion,optimizer,train_dataloader)
-            print(loss)
+            train_loss,train_metric = training_epoch(model,criterion,optimizer,train_dataloader)
             #Validation
-            loss,metric = valid_epoch(model,valid_dataloader)
+            val_loss,val_metric = valid_epoch(model,criterion,valid_dataloader)
             
             #Logging + saving model
-            pass
+            writer.add_scalar('Training loss (CE)',
+                            train_loss,
+                            epoch)
+            writer.add_scalar('Validation loss (CE)',
+                            val_loss,
+                            epoch)
 
+
+            for j,c in enumerate(CLASSES):
+                writer.add_scalar(f'Train F1 Scores {c}',
+                                    train_metric[j],
+                                    epoch)
+                                    
+            for j,c in enumerate(CLASSES):
+                writer.add_scalar(f'Validation F1 Scores {c}',
+                                    val_metric[j],
+                                    epoch)
+                
+            print(f"Training Loss: {train_loss} \tValid Loss: {val_loss}")
+
+            # if i==0:
+            #     tracker.epoch_end()
+            #     tracker.stop()
+        writer.close() 
+        
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
